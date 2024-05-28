691465537cfdc71e77a205ab8813dbcb2f4916c9
moss <moss.huai@searching-info.com>
2024-05-28 17:47:46 +0800

    1

--- lf_test/lf_boss/lf_boss.py
+++ lf_test/lf_boss/lf_boss.py
@@ -1,12 +1,28 @@
 import requests
 import execjs
+session = requests.Session()
+
+url = "https://www.zhipin.com/wapi/zpgeek/search/joblist.json?scene=1&query=Java&city=101010100&experience=&payType=&partTime=&degree=&industry=&scale=&stage=&position=&jobType=&salary=&multiBusinessDistrict=&multiSubway=&page=1&pageSize=30"
+
+payload = {}
+headers = {
+  'accept': 'application/json, text/plain, */*',
+  'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0',
+  'x-requested-with': 'XMLHttpRequest'
+}
+response = session.request("GET", url, headers=headers, data=payload)
+print(response.json())
+name= response.json()['zpData']['name']
+seed= response.json()['zpData']['seed']
+ts= response.json()['zpData']['ts']
 
 jscode = open("1.js", "r", encoding="utf-8").read()
 exec_js = execjs.compile(jscode)
 
-zse96_md5 = exec_js.call('justmd5', pl_url, cookie_string)
+#zse96_md5 = exec_js.call('justmd5', pl_url, cookie_string)
 
-session = requests.Session()
+
 
 url = "https://www.zhipin.com/wapi/zpgeek/search/joblist.json?scene=1&query=Java&city=101010100&experience=&payType=&partTime=&degree=&industry=&scale=&stage=&position=&jobType=&salary=&multiBusinessDistrict=&multiSubway=&page=1&pageSize=30"
 
